4cd7d5abe5c743b17810379ce3e5a3029e6c2e8a
vicente.espineira <vicente.espineira@gmail.com>
2023-03-16 12:05:51 +0100

    primer commit

---
+++ src/main/java/es/mde/acing/proyectoLibEspineira/ProyectoLibEspineiraApplication.java
@@ -0,0 +1,13 @@
+package es.mde.acing.proyectoLibEspineira;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoLibEspineiraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoLibEspineiraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/es/mde/acing/proyectoLibEspineira/ProyectoLibEspineiraApplicationTests.java
@@ -0,0 +1,13 @@
+package es.mde.acing.proyectoLibEspineira;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoLibEspineiraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
